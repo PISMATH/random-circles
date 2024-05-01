885f43c215ec4d0fba783c701b1afb3c80b68112
PISMATH <sai.bhushan2019@gmail.com>
2024-05-01 10:25:13 +0800

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+Point = tuple[int, int]
+Circle = tuple[Point, Point, int]
+Line = tuple[Point, Point]
+Object = Circle | Line
+
+points: list[Point] = []
+lines: list[Line] = []
+circles: list[Circle] = []
+
+def intersectLines(line1, line2):
+    pass
+
+def intersectCircle(circle1, circle2):
+    pass
+
+def intersectLineCircle(line1, circle1):
+    pass
